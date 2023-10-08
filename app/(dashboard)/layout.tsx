1138df1bd1a81c7c752ca9872c039bcb97faffeb
Adit0507 <adi05072002@gmail.com>
2023-10-08 14:04:46 +0530

    header layout done

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,8 +6,12 @@ import { UserButton } from "@clerk/nextjs";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav>
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
         <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
       </nav>
       <main className="flex w-full flex-grow">{children}</main>
     </div>
