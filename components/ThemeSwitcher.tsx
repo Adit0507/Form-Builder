3c7cb689124adc40c95ceeb1f5ce43bd253669d4
Adit0507 <adi05072002@gmail.com>
2023-10-08 13:30:35 +0530

    theme switch

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./providers/ThemeProvider";
+import { useTheme } from "next-themes";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { DesktopIcon, SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 const ThemeSwitcher = () => {
-  return (
-    <div>ThemeSwitcher</div>
-  )
-}
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
 
-export default ThemeSwitcher
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border">
+        <TabsTrigger value="light" onClick={() => setTheme("light")}>
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+
+        <TabsTrigger value="light" onClick={() => setTheme("dark")}>
+          <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+
+        <TabsTrigger value="light" onClick={() => setTheme("system")}>
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+};
+
+export default ThemeSwitcher;
