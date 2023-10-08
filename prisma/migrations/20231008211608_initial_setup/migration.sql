fb8464ae4afd4c4bc70eb20c016dfaf7b7ffe4ff
Adit0507 <adi05072002@gmail.com>
2023-10-09 02:48:39 +0530

    Prisma setup

---
+++ prisma/migrations/20231008211608_initial_setup/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "content" TEXT NOT NULL DEFAULT '[]',
+    "visits" INTEGER NOT NULL DEFAULT 0,
+    "submissions" INTEGER NOT NULL DEFAULT 0,
+    "shareURL" TEXT NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FormSubmissions" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "formId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "FormSubmissions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_shareURL_key" ON "Form"("shareURL");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_name_userId_key" ON "Form"("name", "userId");
+
+-- AddForeignKey
+ALTER TABLE "FormSubmissions" ADD CONSTRAINT "FormSubmissions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
