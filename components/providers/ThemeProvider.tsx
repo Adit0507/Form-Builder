ef42a2c94b81a603f8a0fa435ba20da4e4bb4e12
Adit0507 <adi05072002@gmail.com>
2023-10-08 12:59:24 +0530

    smol fix

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemeProvider {...props}> {children} </NextThemeProvider>;
