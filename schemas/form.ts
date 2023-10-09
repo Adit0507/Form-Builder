5e0f34e80229d0eea60d71f69392c7a5f76b6497
Adit0507 <adi05072002@gmail.com>
2023-10-09 22:57:21 +0530

    form schema

---
+++ schemas/form.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  name: z.string().min(4),
+  description: z.string().optional(),
+});
+
+export type formSchemaType = z.infer<typeof formSchema>;
