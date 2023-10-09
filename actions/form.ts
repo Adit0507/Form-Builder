193cadee818745233ad6a66ecbf220c0651a32dc
Adit0507 <adi05072002@gmail.com>
2023-10-09 13:24:35 +0530

    Stats card done

---
+++ actions/form.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs";
+class UserNotFoundError extends Error {}
+import prisma from "@/lib/prisma";
+
+export async function GetFormStats() {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundError();
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = stats._sum.visits || 0;
+  const submissions = stats._sum.submissions || 0;
+
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return { visits, submissions, submissionRate, bounceRate };
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import { GetFormStats } from "@/actions/form";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import React, { ReactNode, Suspense } from "react";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import {TbArrowBounce} from "react-icons/tb"
 
 const Home = () => {
   return (
-    <div>Hiii</div>
-  )
+    <div>
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+
+      <Separator className="my-6" />
+    </div>
+  );
+};
+
+async function CardStatsWrapper() {
+  const stats = await GetFormStats();
+
+  return <StatsCards loading={false} data={stats} />;
 }
 
-export default Home
+interface StatsCardProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        icon={<LuView className="text-blue-600" />}
+        helperText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-blue-600"
+      />
+
+      <StatsCard
+        title="Total submissions"
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time Form Submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />
+
+      <StatsCard
+        title="Submission Rate"
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard
+        title="Bounce Rate"
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leaves without interacting"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-red-600"
+      />
+    </div>
+  );
+}
+
+export function StatsCard({
+  title,
+  value,
+  icon,
+  helperText,
+  loading,
+  className,
+}: {
+  title: string;
+  value: string;
+  icon: ReactNode;
+  helperText: string;
+  loading: boolean;
+  className: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">
+          {title}
+        </CardTitle>
+        {icon}
+      </CardHeader>
+
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Home;
