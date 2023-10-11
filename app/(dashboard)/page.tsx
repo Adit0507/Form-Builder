5af2bf2c9e12262bd8386e61d6565f9785023159
Adit0507 <adi05072002@gmail.com>
2023-10-11 14:01:48 +0530

    create form btn done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -19,7 +19,9 @@ const Home = () => {
       <Separator className="my-6" />
       <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
       <Separator className="my-6" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <CreateFormBtn />
+      </div>
     </div>
   );
 };
---
+++ components/CreateFormBtn.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { formSchema, formSchemaType } from "@/schemas/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+
+import { BsFileEarmarkPlus } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { toast } from "./ui/use-toast";
+import { Textarea } from "./ui/textarea";
+import { CreateForm } from "@/actions/form";
+import { useRouter } from "next/navigation";
+
+const CreateFormBtn = () => {
+  const router = useRouter();
+  const form = useForm<formSchemaType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(values: formSchemaType) {
+    try {
+      const formId = await CreateForm(values);
+
+      toast({
+        title: "Success",
+        description: "Form created successfully",
+      });
+      // after saving
+      router.push(`/builder/${formId}`);  
+    } catch (err) {
+      toast({
+        title: "Error",
+        variant: "destructive",
+        description: "something went wrong, plz try again",
+      });
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant={"outline"}
+          className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
+        >
+          <BsFileEarmarkPlus className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
+          <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">
+            Create New Form
+          </p>
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField 
+              control={form.control}
+              name="description"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea rows={5} {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+
+        <DialogFooter>
+          <Button onClick={form.handleSubmit(onSubmit)} disabled={form.formState.isSubmitting} className="w-full mt-4">
+            {!form.formState.isSubmitting && <span>Save</span>}
+            {form.formState.isSubmitting && <ImSpinner2 className="animate-spin" />}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateFormBtn;
