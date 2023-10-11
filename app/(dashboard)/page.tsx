7f97897acf1d7f1824641b00b9dea48463fac164
Adit0507 <adi05072002@gmail.com>
2023-10-11 21:38:20 +0530

    finally done with the form view

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,26 @@
-import { GetFormStats } from "@/actions/form";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { GetFormStats, GetForms } from "@/actions/form";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import React, { ReactNode, Suspense } from "react";
 import { LuView } from "react-icons/lu";
-import { FaWpforms } from "react-icons/fa";
+import { FaEdit, FaWpforms } from "react-icons/fa";
+import { BiRightArrowAlt } from "react-icons/bi";
 import { HiCursorClick } from "react-icons/hi";
-import {TbArrowBounce} from "react-icons/tb"
+import { TbArrowBounce } from "react-icons/tb";
 import CreateFormBtn from "@/components/CreateFormBtn";
+import { Form } from "@prisma/client";
+import { Badge } from "@/components/ui/badge";
+import { formatDistance } from "date-fns";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -20,7 +33,15 @@ const Home = () => {
       <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
       <Separator className="my-6" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <CreateFormBtn />
+        <CreateFormBtn />
+        {/* love Suspense 💘 */}
+        <Suspense
+          fallback={[1, 2, 3, 4].map((el) => (
+            <FormCardSkeleton key={el} />
+          ))}
+        >
+          <FormCards />
+        </Suspense>
       </div>
     </div>
   );
@@ -117,6 +138,76 @@ export function StatsCard({
         <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
       </CardContent>
     </Card>
+  );
+}
+
+function FormCardSkeleton() {
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
+}
+
+function FormCard({ form }: { form: Form }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant="destructive">Draft</Badge>}
+        </CardTitle>
+
+        <CardDescription>
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {form.published && (
+            <span>
+              <LuView className="text-muted-foreground" />
+              <span>{form.visits.toLocaleString()}</span>
+              <FaWpforms className="text-muted-foreground" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          )}
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+        {form.description || "No descripton"}
+      </CardContent>
+      <CardFooter>
+        {form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4">
+            <Link href={`/forms/${form.id}`}>
+              View submissions
+              <BiRightArrowAlt />
+            </Link>
+          </Button>
+        )}
+
+        {!form.published && (
+          <Button
+            asChild
+            className="w-full text-md mt-2 gap-4"
+            variant={"secondary"}
+          >
+            <Link href={`/builder/${form.id}`}>
+              Edit Form <FaEdit />
+            </Link>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
+
+async function FormCards() {
+  const forms = await GetForms();
+
+  return (
+    <>
+      {forms.map((form) => (
+        <FormCard key={form.id} form={form} />
+      ))}
+    </>
   );
 }
 
