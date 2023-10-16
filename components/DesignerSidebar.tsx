3615f283c3499b324c8380411a267722474ef6f2
Adit0507 <adi05072002@gmail.com>
2023-10-16 11:27:22 +0530

    text field on the sidebar

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FormElements } from './FormElements'
+import SidebarBtnElement from './SidebarBtnElement'
 
 const DesignerSidebar = () => {
   return (
     <aside className='w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full'>
       Elements
+      <SidebarBtnElement formElement={FormElements.TextField} />
     </aside>
   )
 }
