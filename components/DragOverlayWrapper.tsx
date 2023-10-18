d44d0ec7ee56e02167fe3043c8bb48f280587b6f
Adit0507 <adi05072002@gmail.com>
2023-10-18 13:05:18 +0530

    finshed the overlay wrapper

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -2,8 +2,10 @@ import { Active, DragOverlay, useDndMonitor } from "@dnd-kit/core";
 import { useState } from "react";
 import { ElementsType, FormElements } from "./FormElements";
 import { SidebarBtnElementDragOverlay } from "./SidebarBtnElement";
+import useDesigner from "./hooks/useDesigner";
 
 const DragOverlayWrapper = () => {
+  const { elements } = useDesigner();
   const [draggedItem, setDraggedItem] = useState<Active | null>(null);
 
   useDndMonitor({
@@ -25,7 +27,24 @@ const DragOverlayWrapper = () => {
 
   if (isSidebarBtnElement) {
     const type = draggedItem.data?.current?.type as ElementsType;
-    node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />
+    node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />;
+  }
+
+  const isDesignerElement = draggedItem.data?.current?.isDesignerElement;
+  if (isDesignerElement) {
+    const elementId = draggedItem.data?.current?.elementId;
+    const element = elements.find((el) => el.id === elementId);
+
+    if (!element) node = <div>Element not found</div>;
+    else {
+      const DesignerElementComponent = FormElements[element.type].designerComponent;
+
+      node = (
+        <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80 pointer pointer-events-none">
+          <DesignerElementComponent elementInstance={element} />
+        </div>
+      );
+    }
   }
 
   return <DragOverlay>{node}</DragOverlay>;
