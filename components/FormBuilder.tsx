2cd57c18d5d012edb35ee44e9a5a5a66ff389084
Adit0507 <adi05072002@gmail.com>
2023-10-21 19:16:23 +0530

    adding touch && mouse sensor :construction_worker:

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -7,11 +7,26 @@ import PublishFormBtn from "./PublishFormBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import Designer from "./Designer";
 import DragOverlayWrapper from "./DragOverlayWrapper";
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 
 const FormBuilder = ({ form }: { form: Form }) => {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10,
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5
+    }
+  })
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <main className="flex flex-col w-full">
         <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
