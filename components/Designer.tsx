a1eb15177b40c8c046c2f8080b0908b9daca8707
Adit0507 <adi05072002@gmail.com>
2023-10-21 17:51:40 +0530

    moved it up

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -19,72 +19,6 @@ import { idGenerator } from "@/lib/idGenerator";
 import { BiSolidTrash } from "react-icons/bi";
 import { Button } from "./ui/button";
 
-const Designer = () => {
-  const { elements, addElement } = useDesigner();
-
-  const droppable = useDroppable({
-    id: "designer-drop-area",
-    data: {
-      isDesignerPropArea: true,
-    },
-  });
-
-
-  useDndMonitor({
-    onDragEnd: (event: DragEndEvent) => {
-      const { active, over } = event;
-      if (!active || !over) return;
-
-      const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
-
-      if (isDesignerBtnElement) {
-        const type = active.data?.current?.type;
-        const newElement = FormElements[type as ElementsType].construct(
-          idGenerator()
-        );
-
-        addElement(0, newElement);
-      }
-    },
-  });
-
-
-  return (
-    <div className="flex w-full h-full">
-      <div className="p-4 w-full">
-        <div
-          ref={droppable.setNodeRef}
-          className={cn(
-            "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
-            droppable.isOver && "ring-4 ring-primary ring-inset"
-          )}
-        >
-          {!droppable.isOver && elements.length === 0 && (
-            <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
-              Drop Here
-            </p>
-          )}
-
-          {droppable.isOver && (
-            <div className="p-4 w-full">
-              <div className="h-[120px] rounded-md bg-primary/20"></div>
-            </div>
-          )}
-
-          {elements.length > 0 && (
-            <div className="flex flex-col w-full gap-2 p-4">
-              {elements.map((element) => (
-                <DesignerElementWrapper key={element.id} element={element} />
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-      <DesignerSidebar />
-    </div>
-  );
-};
-
 function DesignerElementWrapper({
   element,
 }: {
@@ -183,5 +117,71 @@ function DesignerElementWrapper({
     </div>
   );
 }
+
+const Designer = () => {
+  const { elements, addElement } = useDesigner();
+
+  const droppable = useDroppable({
+    id: "designer-drop-area",
+    data: {
+      isDesignerPropArea: true,
+    },
+  });
+
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+
+      if (isDesignerBtnElement) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+
+        addElement(0, newElement);
+      }
+    },
+  });
+
+
+  return (
+    <div className="flex w-full h-full">
+      <div className="p-4 w-full">
+        <div
+          ref={droppable.setNodeRef}
+          className={cn(
+            "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+            droppable.isOver && "ring-4 ring-primary ring-inset"
+          )}
+        >
+          {!droppable.isOver && elements.length === 0 && (
+            <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+              Drop Here
+            </p>
+          )}
+
+          {droppable.isOver && (
+            <div className="p-4 w-full">
+              <div className="h-[120px] rounded-md bg-primary/20"></div>
+            </div>
+          )}
+
+          {elements.length > 0 && (
+            <div className="flex flex-col w-full gap-2 p-4">
+              {elements.map((element) => (
+                <DesignerElementWrapper key={element.id} element={element} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <DesignerSidebar />
+    </div>
+  );
+};
 
 export default Designer;
