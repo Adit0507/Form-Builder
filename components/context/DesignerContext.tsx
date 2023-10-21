d88565dbe13c8053c3c70d54df8280774846a811
Adit0507 <adi05072002@gmail.com>
2023-10-21 19:25:45 +0530

    adding

--- components/context/DesignerContext.tsx
+++ components/context/DesignerContext.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { ReactNode, createContext, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
 import { FormElementInstance } from "../FormElements";
 
 type DesignerContextType = {
   elements: FormElementInstance[];
   addElement: (index: number, element: FormElementInstance) => void;
   removeElement: (id: string) => void;
+  selectedElement: FormElementInstance | null
+  setSelectedElement: Dispatch<SetStateAction<FormElementInstance |null>> 
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -17,6 +19,7 @@ export default function DesignerContextProvider({
   children: ReactNode;
 }) {
   const [elements, setElements] = useState<FormElementInstance[]>([]);
+  const [selectedElement, setSelectedElement] = useState<FormElementInstance | null>(null)
 
   const addElement = (index: number, element: FormElementInstance) => {
     setElements((prev) => {
@@ -32,7 +35,7 @@ export default function DesignerContextProvider({
   };
 
   return (
-    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+    <DesignerContext.Provider value={{ elements, addElement, removeElement, selectedElement, setSelectedElement }}>
       {" "}
       {children}{" "}
     </DesignerContext.Provider>
