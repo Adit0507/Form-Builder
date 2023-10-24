87ab15e77f2345c9da717fe0b1eccc1166a6fe05
Adit0507 <adi05072002@gmail.com>
2023-10-24 20:40:24 +0530

    fixes

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -8,13 +8,21 @@ async function BuilderPage({
     id: string;
   };
 }) {
-    const {id} = params
-    const form = await GetFormById(Number(id))
+  const { id } = params;
+  const form = await GetFormById(Number(id));
 
-    if(!form){
-        throw new Error("form not found")
-    } 
+  if (!form) {
+    throw new Error("form not found");
+  }
 
+  const {visits, submissions} = form
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
   return <>Details</>;
 }
 
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -22,19 +22,17 @@ import { formatDistance } from "date-fns";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-const Home = () => {
+export default function Home() {
   return (
     <div className="container pt-4">
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
-
       <Separator className="my-6" />
-      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className="my-6" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <CreateFormBtn />
-        {/* love Suspense 💘 */}
         <Suspense
           fallback={[1, 2, 3, 4].map((el) => (
             <FormCardSkeleton key={el} />
@@ -45,11 +43,10 @@ const Home = () => {
       </div>
     </div>
   );
-};
+}
 
 async function CardStatsWrapper() {
   const stats = await GetFormStats();
-
   return <StatsCards loading={false} data={stats} />;
 }
 
@@ -75,14 +72,14 @@ function StatsCards(props: StatsCardProps) {
       <StatsCard
         title="Total submissions"
         icon={<FaWpforms className="text-yellow-600" />}
-        helperText="All time Form Submissions"
+        helperText="All time form submissions"
         value={data?.submissions.toLocaleString() || ""}
         loading={loading}
         className="shadow-md shadow-yellow-600"
       />
 
       <StatsCard
-        title="Submission Rate"
+        title="Submission rate"
         icon={<HiCursorClick className="text-green-600" />}
         helperText="Visits that result in form submission"
         value={data?.submissionRate.toLocaleString() + "%" || ""}
@@ -91,7 +88,7 @@ function StatsCards(props: StatsCardProps) {
       />
 
       <StatsCard
-        title="Bounce Rate"
+        title="Bounce rate"
         icon={<TbArrowBounce className="text-red-600" />}
         helperText="Visits that leaves without interacting"
         value={data?.submissionRate.toLocaleString() + "%" || ""}
@@ -112,20 +109,17 @@ export function StatsCard({
 }: {
   title: string;
   value: string;
-  icon: ReactNode;
   helperText: string;
-  loading: boolean;
   className: string;
+  loading: boolean;
+  icon: ReactNode;
 }) {
   return (
     <Card className={className}>
       <CardHeader className="flex flex-row items-center justify-between pb-2">
-        <CardTitle className="text-sm font-medium text-muted-foreground">
-          {title}
-        </CardTitle>
+        <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
         {icon}
       </CardHeader>
-
       <CardContent>
         <div className="text-2xl font-bold">
           {loading && (
@@ -145,63 +139,8 @@ function FormCardSkeleton() {
   return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
 }
 
-function FormCard({ form }: { form: Form }) {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2 justify-between">
-          <span className="truncate font-bold">{form.name}</span>
-          {form.published && <Badge>Published</Badge>}
-          {!form.published && <Badge variant="destructive">Draft</Badge>}
-        </CardTitle>
-
-        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
-          {formatDistance(form.createdAt, new Date(), {
-            addSuffix: true,
-          })}
-          {form.published && (
-            <span>
-              <LuView className="text-muted-foreground" />
-              <span>{form.visits.toLocaleString()}</span>
-              <FaWpforms className="text-muted-foreground" />
-              <span>{form.submissions.toLocaleString()}</span>
-            </span>
-          )}
-        </CardDescription>
-      </CardHeader>
-
-      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
-        {form.description || "No descripton"}
-      </CardContent>
-      <CardFooter>
-        {form.published && (
-          <Button asChild className="w-full mt-2 text-md gap-4">
-            <Link href={`/forms/${form.id}`}>
-              View submissions
-              <BiRightArrowAlt />
-            </Link>
-          </Button>
-        )}
-
-        {!form.published && (
-          <Button
-            asChild
-            className="w-full text-md mt-2 gap-4"
-            variant={"secondary"}
-          >
-            <Link href={`/builder/${form.id}`}>
-              Edit Form <FaEdit />
-            </Link>
-          </Button>
-        )}
-      </CardFooter>
-    </Card>
-  );
-}
-
 async function FormCards() {
   const forms = await GetForms();
-
   return (
     <>
       {forms.map((form) => (
@@ -211,4 +150,48 @@ async function FormCards() {
   );
 }
 
-export default Home;
+function FormCard({ form }: { form: Form }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {form.published && (
+            <span className="flex items-center gap-2">
+              <LuView className="text-muted-foreground" />
+              <span>{form.visits.toLocaleString()}</span>
+              <FaWpforms className="text-muted-foreground" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+        {form.description || "No description"}
+      </CardContent>
+      <CardFooter>
+        {form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4">
+            <Link href={`/forms/${form.id}`}>
+              View submissions <BiRightArrowAlt />
+            </Link>
+          </Button>
+        )}
+        {!form.published && (
+          <Button asChild variant={"secondary"} className="w-full mt-2 text-md gap-4">
+            <Link href={`/builder/${form.id}`}>
+              Edit form <FaEdit />
+            </Link>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
