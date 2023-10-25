130cb93a2b928fc88531a353ac035b03ac76a5c5
Adit0507 <adi05072002@gmail.com>
2023-10-25 20:33:06 +0530

    added renderkey

--- components/FormSubmitComponent.tsx
+++ components/FormSubmitComponent.tsx
@@ -83,7 +83,7 @@ const FormSubmitComponent = ({
 
   return (
     <div className="flex justify-center w-full h-full items-center p-8">
-      <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
+      <div key={renderKey} className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
         {content.map((element) => {
           const FormElement = FormElements[element.type].formComponent;
           return (
