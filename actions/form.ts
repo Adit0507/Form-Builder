7b353a6af8a65fec607655861329c6af96bebca5
Adit0507 <adi05072002@gmail.com>
2023-10-25 17:55:57 +0530

    get form by content url action

--- actions/form.ts
+++ actions/form.ts
@@ -127,3 +127,19 @@ export async function PublishForm(id: number) {
     },
   });
 }
+
+export async function GetFormContentByUrl(formUrl: string) {
+  return await prisma.form.update({
+    select: {
+      content: true,
+    },
+    data: {
+      visits: {
+        increment: 1,
+      },
+    },
+    where: {
+      shareURL: formUrl,
+    },
+  });
+}
