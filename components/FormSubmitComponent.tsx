34bef2f49c2eee575a154a0ee2ae6944eba536cf
Adit0507 <adi05072002@gmail.com>
2023-10-25 20:26:26 +0530

    form submit done

--- components/FormSubmitComponent.tsx
+++ components/FormSubmitComponent.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 import { HiCursorClick } from "react-icons/hi";
 import { toast } from "./ui/use-toast";
 import { ImSpinner2 } from "react-icons/im";
-
+import { SubmitForm } from "@/actions/form";
 
 const FormSubmitComponent = ({
   formUrl,
@@ -15,30 +15,102 @@ const FormSubmitComponent = ({
   formUrl: string;
   content: FormElementInstance[];
 }) => {
-    const formValues = useRef<{[key: string]: string}>({})
-    const formErrors = useRef<{[key: string]: boolean}>({})
+  const formValues = useRef<{ [key: string]: string }>({});
+  const formErrors = useRef<{ [key: string]: boolean }>({});
 
-    const submitValue = useCallback((key: string, value: string) => {
-        formValues.current[key] = value
-    }, [])
+  const [renderKey, setRenderKey] = useState(new Date().getTime());
+  const [submitted, setSubmitted] = useState(false);
+  const [pending, startTransition] = useTransition();
 
-    const submitForm= async() =>{
-        
+  const validateForm: () => boolean = useCallback(() => {
+    for (const field of content) {
+      const actualValue = formValues.current[field.id] || "";
+      const valid = FormElements[field.type].validate(field, actualValue);
+
+      if (!valid) {
+        formErrors.current[field.id] = true;
+      }
     }
- 
+
+    if (Object.keys(formErrors.current).length > 0) return false;
+
+    return true;
+  }, [content]);
+
+  const submitValue = useCallback((key: string, value: string) => {
+    formValues.current[key] = value;
+  }, []);
+
+  const submitForm = async () => {
+    formErrors.current = {};
+    const validForm = validateForm();
+    if (!validForm) {
+      setRenderKey(new Date().getTime());
+      toast({
+        title: "Error",
+        description: "please check the form for errors",
+        variant: "destructive",
+      });
+
+      return;
+    }
+
+    try {
+      const jsonContent = JSON.stringify(formValues.current);
+      await SubmitForm(formUrl, jsonContent);
+      setSubmitted(true);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "somethin went wrong!",
+        variant: "destructive",
+      });
+    }
+  };
+
+  if (submitted) {
+    return (
+      <div className="flex justify-center w-full h-full items-center p-8">
+        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
+          <h1 className="text-2xl font-bold">Form submitted</h1>
+          <p className="text-muted-foreground">
+            Thank u for submitting the form, you can close this page now
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-center w-full h-full items-center p-8">
       <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
         {content.map((element) => {
           const FormElement = FormElements[element.type].formComponent;
-          return <FormElement key={element.id} elementInstance={element} submitValue={submitValue} />;
+          return (
+            <FormElement
+              key={element.id}
+              elementInstance={element}
+              submitValue={submitValue}
+              isInvalid={formErrors.current[element.id]}
+              defaultValue={formValues.current[element.id]}
+            />
+          );
         })}
 
-        <Button className="mt-8" onClick={() => {
-            submitForm()
-        }} >
-          <HiCursorClick className="mr-2" />
-          Submit
+        <Button
+          className="mt-8"
+          onClick={() => {
+            startTransition(submitForm);
+          }}
+          disabled={pending}
+        >
+          {!pending && (
+            <>
+              <HiCursorClick className="mr-2" />
+              Submit
+            </>
+          )}
+          {pending && <ImSpinner2 className="animate-spin" />}
         </Button>
       </div>
     </div>
