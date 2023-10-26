61f9564c7f5974fab17f7209cce0584d2ccf48d3
Adit0507 <adi05072002@gmail.com>
2023-10-26 21:06:31 +0530

    temp shadcn/ui fix

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -14,10 +14,10 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
-  className,
+  
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
+  <SheetPrimitive.Portal {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
